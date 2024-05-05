13b8e9f1ca2107e415356cf670ab3f886673f3f8
MANAS GUPTA <120665112+manasgupta15@users.noreply.github.com>
2024-05-05 06:30:08 +0000

    string concept

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,9 @@
+const name = "hitesh";
+const repoCount = 50;
+
+console.log(name  + repoCount + "Value");  // not prefer as it is old
+
+console.log(`Hello my name is: ${name} and my repocount is : ${repoCount}`); // interpolation -> modern way
+
+const game = new String("Hello World");
+console.log(game);
