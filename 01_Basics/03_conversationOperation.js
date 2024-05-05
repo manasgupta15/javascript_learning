4cdb50a7671562293e69374b2dca3f559d069893
MANAS GUPTA <120665112+manasgupta15@users.noreply.github.com>
2024-05-05 05:40:07 +0000

    update

--- 01_Basics/03_conversationOperation.js
+++ 01_Basics/03_conversationOperation.js
@@ -1,18 +1,18 @@
-let score = "33abc";
-console.log(typeof score);
-console.log(typeof(score));
+// let score = "33abc";
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueInNUmber = Number(score);
-console.log(typeof valueInNUmber);
-console.log(valueInNUmber);
-console.log(typeof(NaN));  // number
+// let valueInNUmber = Number(score);
+// console.log(typeof valueInNUmber);
+// console.log(valueInNUmber);
+// console.log(typeof(NaN));  // number
 
-console.log("*******************");
-let score2 = null;
-console.log(typeof score2);
-let valueInNUmber2 = Number(score2);
-console.log(typeof valueInNUmber2);
-console.log(valueInNUmber2);
+// console.log("*******************");
+// let score2 = null;
+// console.log(typeof score2);
+// let valueInNUmber2 = Number(score2);
+// console.log(typeof valueInNUmber2);
+// console.log(valueInNUmber2);
 
 
 // when we convert it into number...
@@ -25,3 +25,40 @@ console.log(valueInNUmber2);
 // 1 => true
 // 0 => false
 // "manas" => true
+
+// *******************operation********
+
+// let value = 3
+// let negVal = -value;
+// console.log(negVal);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/3);
+// console.log(2%3);
+
+// let str1 = "Hello";
+// let str2 = " Manas";
+// // follow left to right rule..
+// console.log(str1 + str2);
+// console.log("1" + 2);  // 12
+// console.log(1 + "2");  // 12
+// console.log("1" + "2"); // 12
+// console.log("1" + 2 + 2); // 122
+// console.log(1 + 2 + "2");  // 32
+
+// console.log(3 + 4 * 5 % 3);
+
+// console.log(+true); // 1
+// // console.log(true+); // not possible
+// console.log(+""); // not prefer
+
+// not prefer
+// let num1,num2,num3;
+// num1 = num2 = num3 = 2+2;
+// console.log(num1);
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
